5dfe50f370340f18679fd1a5f07fe13f83ea2621
lourdesashleyy <142953562+lourdesashleyy@users.noreply.github.com>
2023-10-26 17:24:27 +0800

    Delete SantosLAITELEC1C directory

--- SantosLAITELEC1C/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SantosLAITELEC1C.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
